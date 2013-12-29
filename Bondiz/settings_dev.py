e6354336925bcbb8df299f85dd1045385a12d117
AndreiDev <andrei.averkin@gmail.com>
2013-12-29 23:28:02 +0200

    Ubuntu first commit

--- Bondiz/settings_dev.py
+++ Bondiz/settings_dev.py
@@ -8,7 +8,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'bondizDB',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
+        'USER': 'andrei',
         'PASSWORD': 'psar5248',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
@@ -32,7 +32,7 @@ STATICFILES_DIRS = (
 )
 
 TEMPLATE_DIRS = (
-    "c:/andrei/workspace/github/bondiz/templates",
+    "/home/andrei/Workspace/Bondiz/templates",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
