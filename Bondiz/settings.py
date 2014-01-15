ab4ec1381d2f1b21b13166418d54edb20036e13d
AndreiDev <andrei.averkin@gmail.com>
2014-01-15 22:35:07 +0200

    1st deploy to heroku

--- Bondiz/settings.py
+++ Bondiz/settings.py
@@ -198,6 +198,26 @@ EMAIL_HOST_PASSWORD = 'a-SAR5248'
 from django.core.urlresolvers import reverse_lazy
 LOGIN_URL = reverse_lazy('login')
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 try:
     from settings_dev import *
 except ImportError, exp:
