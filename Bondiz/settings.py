a08b9ec3825a77a7a00e4c38c9e0418bcb163aa5
AndreiDev <andrei.averkin@gmail.com>
2014-01-16 21:51:07 +0200

    updated templates_dir again

--- Bondiz/settings.py
+++ Bondiz/settings.py
@@ -116,10 +116,17 @@ TEMPLATE_DIRS = (
 )
 
 import os.path
-PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_DIR, "templates"),
-    # here you can add another templates directory if you wish.
+    root('templates'),
 )
 
 INSTALLED_APPS = (
