51249cd29fc0ac2a04851822075be778f600e599
AndreiDev <andrei.averkin@gmail.com>
2014-01-17 21:36:50 +0200

    commented out original DB settings

--- Bondiz/settings.py
+++ Bondiz/settings.py
@@ -9,17 +9,17 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'd4it4mvasg085c',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'wmidifkpduzeon',
-        'PASSWORD': 'pSuNswaXi6XU6K7mDplPocW5aF',
-        'HOST': 'ec2-54-204-32-91.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'd4it4mvasg085c',                      # Or path to database file if using sqlite3.
+#        # The following settings are not used with sqlite3:
+#        'USER': 'wmidifkpduzeon',
+#        'PASSWORD': 'pSuNswaXi6XU6K7mDplPocW5aF',
+#        'HOST': 'ec2-54-204-32-91.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#        'PORT': '5432',                      # Set to empty string for default.
+#    }
+#}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
