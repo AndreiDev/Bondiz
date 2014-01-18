beef81f7936398c3ff9fb2af585e1a97dea68585
AndreiDev <andrei.averkin@gmail.com>
2014-01-18 14:17:50 +0200

    changed logging to console

--- emailDebug.py
+++ emailDebug.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 def emailDebug(message):
-    f = open('emailLog.txt','a')
-    f.write(str(datetime.now()) + ' | ' + message + '\n')
-    f.close()
-    #print(str(datetime.now()) + ' | ' + message)
+    #f = open('emailLog.txt','a')
+    #f.write(str(datetime.now()) + ' | ' + message + '\n')
+    #f.close()
+    print(str(datetime.now()) + ' | ' + message)
--- taskDebug.py
+++ taskDebug.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 def taskDebug(message):
-    f = open('taskLog.txt','a')
-    f.write(str(datetime.now()) + ' | ' + message + '\n')
-    f.close()
-    #print(str(datetime.now()) + ' | ' + message)
+    #f = open('taskLog.txt','a')
+    #f.write(str(datetime.now()) + ' | ' + message + '\n')
+    #f.close()
+    print(str(datetime.now()) + ' | ' + message)
